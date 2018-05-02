61ad78efb8cdcc6a3ac8e7b9980e70b5c7c1a21c
Eric <gzcisco720@gmail.com>
2018-05-02 15:32:00 +1000

    project init

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using myDotnetApp.API.Model;
+
+namespace myDotnetApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+        
+        public DbSet<Values> Values { get; set; }
+    }
+}
---
+++ Model/Values.cs
@@ -0,0 +1,8 @@
+namespace myDotnetApp.API.Model
+{
+    public class Values
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
