c6ab3253210cefe1b09f9605f83660d077b36218
Eric <gzcisco720@gmail.com>
2018-05-08 14:13:49 +1000

    new seed

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -7,7 +7,7 @@ namespace myDotnetApp.API.Helpers
     {
         public static void AddApplicationErrors(this HttpResponse response, string message)
         {
-            response.Headers.Add("Appliction-Error", message);
+            response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
