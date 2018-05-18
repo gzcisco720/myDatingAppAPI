89d844103d12ef706a310ba5e7485b783ad4ae75
Eric <gzcisco720@gmail.com>
2018-05-18 11:31:52 +1000

    API updating with warnings

--- Helpers/UserParams.cs
+++ Helpers/UserParams.cs
@@ -11,5 +11,9 @@ namespace myDotnetApp.API.Helpers
             get { return pageSize; }
             set { pageSize = (value > MaxPageSize)? MaxPageSize:value;}
         }
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
